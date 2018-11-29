3507f1d875924bc0b196e0850ab3997960872cf7
Dario <daaar@users.noreply.github.com>
2018-11-29 17:24:51 +0100

    Update courseCategory.js

--- assets/script/courseCategory.js
+++ assets/script/courseCategory.js
@@ -1,12 +1,10 @@
 $(document).ready(ready);
 
 function ready(){
-    console.log("I'm ready!");
     var id=1;
     
     $.ajax({
         method: "POST",
-        //dataType: "json", //type of data
         crossDomain: true, //localhost purposes
         url: "http://frankgym.altervista.org/php/getCoursesCategory.php", //Relative or absolute path to file.php file
         data: {course:id},
@@ -17,19 +15,14 @@ function ready(){
            var elems = $('.cat');
            
            for(var i = 0; i<category.length ;i++) {
-           //for(i=0, k=0;i<courses.length,k<courses.length;i=i+count+1,k++) {
-           //count = 0;
-           		console.log(elems.eq(i));
                 var url = "url(" + "'" + category[i].url + "'" + ")";
            		elems.eq(i).css("backgroundImage", url);
-                console.log(category[i].Title);
                 /*elems*/$('.Title').eq(i).text(function(){
     				$(this).wrapInner('<a href="http://frankgym.altervista.org/category.html?idCategory='+category[i].ID+'">'+category[i].Title+"<br>"+'</a>')});
 				} 
         },
         error: function(request,error) 
         {
-            console.log("Error");
         }
     });
 
@@ -38,12 +31,10 @@ function ready(){
 $(document).ready(courses);
 
 function courses(){
-    console.log("I'm ready!");
     var id=1;
     
     $.ajax({
         method: "POST",
-        //dataType: "json", //type of data
         crossDomain: true, //localhost purposes
         url: "http://frankgym.altervista.org/php/getCourses.php", //Relative or absolute path to file.php file
         data: {course:id},
@@ -56,26 +47,17 @@ function courses(){
            
            
             for(var k = 0; k<6 ;k++){
-           for(var i = 0; i<category.length ;i++) {
-           //for(i=0, k=0;i<courses.length,k<courses.length;i=i+count+1,k++) {
-           //count = 0;
-			
-            
-            if($('.Title').eq(k).text() == category[i].category)
-            {
-            	$('.shortDescription').eq(k).append(function(){
-    				$(this).wrapInner('<a href="http://frankgym.altervista.org/course.html?idCourse='+category[i].id+'">'+category[i].title+"<br>"+'</a>')});
-            }
-            
-
-
-               
-				}
-                }
+            	for(var i = 0; i<category.length ;i++) {
+		    if($('.Title').eq(k).text() == category[i].category)
+		    {
+			$('.shortDescription').eq(k).append(function(){
+				$(this).wrapInner('<a href="http://frankgym.altervista.org/course.html?idCourse='+category[i].id+'">'+category[i].title+"<br>"+'</a>')});
+            		}
+	 	    }
+              }
         },
         error: function(request,error) 
         {
-            console.log("Error");
         }
     });
 
